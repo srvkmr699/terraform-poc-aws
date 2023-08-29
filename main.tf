b6bd2544bf905124cd5f8b7296f8f072de65a814
srvkmr699 <49115510+srvkmr699@users.noreply.github.com>
2023-08-29 18:02:02 +0530

    Update7 main.tf

--- main.tf
+++ main.tf
@@ -54,7 +54,6 @@ resource "aws_security_group" "metyis_ec2_sg1" {
 resource "aws_instance" "metyis-ec2-instane" {
   ami = lookup(var.awsprops, "ami")
   instance_type = lookup(var.awsprops, "itype")
-  subnet_id = lookup(var.awsprops, "subnet")
   associate_public_ip_address = lookup(var.awsprops, "publicip")
   key_name = lookup(var.awsprops, "keyname")
 
