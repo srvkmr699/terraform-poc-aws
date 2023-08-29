e018765e980ea08fa6111778d8265753be9bb18f
srvkmr699 <srvkmr699@gmail.com>
2023-08-29 17:03:39 +0530

    fix for PR

--- main.tf
+++ main.tf
@@ -59,7 +59,7 @@ resource "aws_security_group" "metyis_ec2_sec_group" {
 resource "aws_instance" "metyis-tf-instane" {
   ami = lookup(var.awsprops, "ami")
   instance_type = lookup(var.awsprops, "itype")
-  subnet_id = lookup(var.awsprops, "subnet") #FFXsubnet2
+  subnet_id = lookup(var.awsprops, "subnet")
   associate_public_ip_address = lookup(var.awsprops, "publicip")
   key_name = lookup(var.awsprops, "keyname")
 
